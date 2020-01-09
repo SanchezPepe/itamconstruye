52ee734eb9fd995e5644cce18edf6f8cb72e2539
sanchezpepe <sanchezpepe97@gmail.com>
2020-01-09 00:37:26 -0600

    social networks added

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,10 +1,19 @@
 <template>
     <footer class="footer has-background-black">
         <div class="content has-text-centered">
-            <p>
-                <strong style="color: white">ITAM Construye 2020</strong>
-            </p>
+            <a href="http://www.itamconstruye.itam.mx/">
+                <strong style="color: white">ITAM Construye - 2020</strong>
+            </a>
         </div>
+        <a href="https://www.facebook.com/ITAMConstruye/">
+            <img class="icons" src="https://bit.ly/2FvRUKz" alt="">
+        </a>
+        <a href="https://twitter.com/ITAMConstruye">
+            <img class="icons" src="https://bit.ly/2FzTewg" alt="">
+        </a>
+        <a href="https://twitter.com/ITAMConstruye">
+            <img class="icons" src="https://bit.ly/37SsTWq" alt="">
+        </a>
     </footer>
 </template>
 
@@ -20,4 +29,9 @@
 </script>
 
 <style>
+    .icons {
+        margin: 0px 20px 0px 20px;
+        max-width: 2.5%;
+        height: auto;
+    }
 </style>
