a7f22d3ede727c320136f082a0c030e855223e63
sanchezpepe <sanchezpepe97@gmail.com>
2020-01-24 18:47:38 -0600

    admin view fix

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,45 +1,65 @@
 <template>
-    <section>
-        <section class="hero is-info is-bold">
+    <section class="has-background-primary">
+        <link rel="stylesheet" href="https://cdn.materialdesignicons.com/2.5.94/css/materialdesignicons.min.css">
+        <section class="hero is-dark is-bold">
             <div class="hero-body">
                 <div class="container">
                     <h1 class="title">
-                        Administración de alumnos
+                        Registro de alumnos
                     </h1>
-                    <h2 class="subtitle">
-                        Consulta, alta, baja y modificación de alumnos
-                    </h2>
                 </div>
             </div>
         </section>
-        <!-- Botones para confirmar o cancelar -->
-        <section class="block">
-            <h1 class="subtitle">Acciones</h1>
-            <div class="buttons container is-centered are-medium">
-                <b-button class="has-text-weight-bold" type="is-info">
-                    <router-link to="/" class="has-text-white">Alumnos</router-link>
-                </b-button>
-                <b-button class="has-text-weight-bold" type="is-info">
-                    <router-link to="/" class="has-text-white">Configuración perfil</router-link>
-                </b-button>
-                <b-button class="has-text-weight-bold" type="is-danger">
-                    <router-link to="/" class="has-text-white">Cerrar Sesión</router-link>
-                </b-button>
-            </div>
-        </section>
-    </section>
 
+        <section class="container">
+            <div class="columns">
+                <div class="column is-three-fifths box">
+                    <p class="subtitle has-text-black">Acciones</p>
+                    <div class="buttons">
+                        <button class="button is-info is-medium">Administrar usuarios</button>
+                        <button class="button is-info is-medium">Información alumnos</button>
+
+                    </div>
+
+                </div>
+                <div class="column box">
+                    <p class="subtitle has-text-black">Información del Usuario</p>
+                    <button class="button is-medium is-info">Editar perfil</button>
+
+                </div>
+            </div>
+            <section>
+                <students></students>
+            </section>
+
+        </section>
+
+    </section>
 </template>
 
 <script>
-    export default {
+    import students from '../components/StudentTable'
 
+    export default {
+        name: 'AdminView',
+        components: {
+            students
+        },
+        methods: {}
     }
 </script>
 
-<style>
-.block{
-    padding: 3%;
-}
+<style scoped>
+    .subtitle {
+        font-weight: bold;
+    }
 
+    .checkbox {
+        padding-top: 10%;
+    }
+
+    .box {
+        padding: 3.5%;
+        margin: 1.5%;
+    }
 </style>
